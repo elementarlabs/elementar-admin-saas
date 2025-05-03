3cbafe490bcb0aeefc8142242f025f075860b810
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-05-03 19:06:54 +0200

    add environment config data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  siteUrl: 'https://saas.elementarui.com',
+  googleAnalyticsId: 'G-LZYWG6MYJ4',
+  pageTitle: 'Elementar Admin / SaaS layout',
+  pageMeta: {
+    keywords: [],
+    description: '',
+    author: ''
+  }
+};
