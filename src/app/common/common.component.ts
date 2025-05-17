ead5fd7bd604102e65fdb5cfefeb80a2f9d5bea8
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-05-17 22:11:14 +0200

    add side panel

--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@/app/header/header.component';
 import { SidebarComponent } from '@/app/sidebar/sidebar.component';
-import { LayoutBodyComponent, LayoutComponent, LayoutSidebarComponent } from '@elementar-ui/components/layout';
+import {
+  LayoutBodyComponent,
+  LayoutComponent,
+  LayoutSidebarComponent
+} from '@elementar-ui/components/layout';
 import { PanelBodyComponent, PanelComponent, PanelHeaderComponent } from '@elementar-ui/components/panel';
+import { SidePanelComponent, SidePanelTabComponent } from '@elementar-ui/components/side-panel';
 
 @Component({
   imports: [
@@ -15,7 +20,9 @@ import { PanelBodyComponent, PanelComponent, PanelHeaderComponent } from '@eleme
     LayoutSidebarComponent,
     PanelBodyComponent,
     PanelComponent,
-    PanelHeaderComponent
+    PanelHeaderComponent,
+    SidePanelTabComponent,
+    SidePanelComponent
   ],
   templateUrl: './common.component.html',
   styleUrl: './common.component.scss'
